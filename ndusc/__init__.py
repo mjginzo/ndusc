b64cad42478d1d6f1c73cb98e46ec3f747560c6b
jorge <jorge.rodriguez.veiga@gmail.com>
2017-12-12 19:54:02 +0100

    Change main __init__.py.

--- ndusc/__init__.py
+++ ndusc/__init__.py
@@ -1,8 +1,24 @@
-"""This is a namespace package."""
-from ndusc.problem_nd.problem import Problem
-from ndusc.cuts_nd.cuts import Cuts
-from ndusc.input_module import Input_module
-# from ndusc.nd import nested_decomposition
-from ndusc.node import Node
-# from ndusc.output_module import Ouput_module
-from ndusc.tree import Tree
+"""Package namespace."""
+import importlib as _importlib
+import pkgutil as _pkgutil
+
+
+def import_submodules(package, recursive=True):
+    """Import all submodules of a module, recursively, including subpackages.
+
+    :param package: package (name or actual module)
+    :type package: str | module
+    :rtype: dict[str, types.ModuleType]
+    """
+    if isinstance(package, str):
+        package = _importlib.import_module(package)
+    results = {}
+    for loader, name, is_pkg in _pkgutil.walk_packages(package.__path__):
+        full_name = package.__name__ + '.' + name
+        results[full_name] = _importlib.import_module(full_name)
+        if recursive and is_pkg:
+            results.update(import_submodules(full_name))
+    return results
+
+
+import_submodules(__name__)
