f79fc3859e5e1b768e76805690301e113646e4be
jorge <jorge.rodriguez.veiga@gmail.com>
2017-12-14 10:45:47 +0100

    Update name space documentation.

--- ndusc/__init__.py
+++ ndusc/__init__.py
@@ -1,14 +1,24 @@
-"""Package namespace."""
+# -*- coding: utf-8 -*-
+"""Package namespace.
+
+This package structure is compatible with Python2 and Python3.
+"""
+
+# Python packages
 import importlib as _importlib
 import pkgutil as _pkgutil
 
 
-def import_submodules(package, recursive=True):
+# _import_submodules ----------------------------------------------------------
+def _import_submodules(package, recursive=True):
     """Import all submodules of a module, recursively, including subpackages.
 
-    :param package: package (name or actual module)
-    :type package: str | module
-    :rtype: dict[str, types.ModuleType]
+    Args:
+        package (:obj:`str`): package (name or actual module).
+        recursive (:obj:`bool`): ``True`` to import subpackages.
+
+    Return:
+        :obj:`dict`: dictionary with package information.
     """
     if isinstance(package, str):
         package = _importlib.import_module(package)
@@ -17,8 +27,10 @@ def import_submodules(package, recursive=True):
         full_name = package.__name__ + '.' + name
         results[full_name] = _importlib.import_module(full_name)
         if recursive and is_pkg:
-            results.update(import_submodules(full_name))
+            results.update(_import_submodules(full_name))
     return results
+# --------------------------------------------------------------------------- #
 
 
-import_submodules(__name__)
+# Execute the _import_submodules function to load the package.
+_import_submodules(__name__)
