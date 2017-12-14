35f509b0876b2b23997b9b82cb200b884ebe5f17
jorge <jorge.rodriguez.veiga@gmail.com>
2017-12-14 10:15:53 +0100

    Update.

--- ndusc/output_module.py
+++ ndusc/output_module.py
@@ -1,18 +1,13 @@
-class Ouput_module(Object):
+class Ouput_module(object):
     path_output=""
     kind_format=""
     name_convergence_graph=""
 
-    def __init__(self, path, format):
-        self.path_output = path
-        self.kind_format = format
+    # def __init__(self, path, format):
+    #     self.path_output = path
+    #     self.kind_format = format
 
-    def plot_convergence_graph():
-
-
-    def plot_tree_graph():
-
-    return
+    # def plot_convergence_graph():
 
 
-    pass
+    # def plot_tree_graph():
